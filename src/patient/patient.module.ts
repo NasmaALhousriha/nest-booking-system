251b1bfadf46fd67228dbc8891f8b5974e3edb01
NasmaNas1 <nasmaaina@gmail.com>
2026-09-15 09:20:07 +0300

    patient_controller

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PatientService } from './patient.service.js';
+import { PatientController } from './patient.controller.js';
 
 @Module({
   providers: [PatientService],
+  controllers: [PatientController],
   exports: [PatientService],
 })
 export class PatientModule {}
